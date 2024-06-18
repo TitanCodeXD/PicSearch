2970d4e762e159574651984af9608314209a83e3
Wesley Santos <wesleysantos32892653@outlook.com>
2024-06-17 21:27:13 -0300

    Obtendo chave  da API e fazendo testes iniciais com API para ver se está tudo certo

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 // React / Hooks
 import { useState, useEffect } from "react";
+import axios from "axios";
 
 //Components
 import SearchBar from "./components/SearchBar/SearchBar";
@@ -10,6 +11,25 @@ import ZoomPhoto from "./components/ZoomPhoto/ZoomPhoto";
 import "./App.css";
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [categoria, setCategoria] = useState("");
+
+  const fetchData = async ([query, categoria]) => {
+    const apiKey = import.meta.env.VITE_UNSPLASH_API_KEY;
+
+    const response = await axios.get("https://api.unsplash.com/photos/random", {
+      params: {
+        client_id: apiKey,
+      },
+    });
+
+    console.log(response);
+  };
+
+  useEffect(() => {
+    fetchData(query, categoria);
+  }, []);
+
   return (
     <>
       <div className="container">
