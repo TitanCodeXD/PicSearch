881ce5b2f14dbebe5c04f01c98c8e60dfbc953ef
Wesley Santos <wesleysantos32892653@outlook.com>
2024-06-22 00:17:45 -0300

    Implementando opção de download de imagem

--- src/components/ZoomPhoto/ZoomPhoto.jsx
+++ src/components/ZoomPhoto/ZoomPhoto.jsx
@@ -1,14 +1,49 @@
 //Hooks
-import React from "react";
+import React, { useRef } from "react";
+
+import { createApi } from "unsplash-js";
 
 //CSS
 import "./ZoomPhoto.css";
 
 const ZoomPhoto = ({ photo, setPhotoZoom }) => {
+  const imageDownloadRef = useRef(null);
+
+  const downloadImage = async (photo) => {
+    try {
+      const imgUrl = `${photo.urls.full}&client_id=${
+        import.meta.env.VITE_UNSPLASH_API_KEY
+      }`;
+      const response = await fetch(imgUrl);
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const blob = await response.blob();
+
+      // Configuração do link de download
+      const url = window.URL.createObjectURL(blob);
+      imageDownloadRef.current.href = url;
+      imageDownloadRef.current.download = `${photo.id}.jpg`;
+
+      // Forçar o clique no link para iniciar o download
+      imageDownloadRef.current.click();
+
+      // Revogar a URL do objeto blob
+      window.URL.revokeObjectURL(url);
+    } catch (error) {
+      console.error("Error fetching download link:", error);
+    }
+  };
+
   return (
     <div className="photo-zoom" onClick={() => setPhotoZoom(null)}>
-      <div className="photo-zoom-container">
+      <div
+        className="photo-zoom-container"
+        onClick={(e) => e.stopPropagation()}
+      >
         <img src={photo.urls.regular} alt={photo.alt_description} />
+        <a ref={imageDownloadRef} className="hidden"></a>
+        <button onClick={() => downloadImage(photo)}>Download</button>
       </div>
     </div>
   );
