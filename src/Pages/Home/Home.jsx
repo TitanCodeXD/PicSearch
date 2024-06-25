411905d113c837b7178ba9b2e170efbb2193a8a2
Wesley Santos <wesleysantos32892653@outlook.com>
2024-06-25 05:01:17 -0300

    Removendo inutilidades e acrescentando quadro de informações da imagem, como seu fotógrafo e sua descrição

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -44,7 +44,7 @@ function App() {
     const response = await axios.get("https://api.unsplash.com/photos/random", {
       params: {
         client_id: apiKey,
-        count: 0,
+        count: 6,
       },
     });
 
@@ -67,8 +67,6 @@ function App() {
   return (
     <>
       <div className="container">
-        <h1>PicSearch</h1>
-        <p>An images website</p>
         <div className="comp">
           <SearchBar
             setQuery={setQuery}
--- src/components/ZoomPhoto/ZoomPhoto.jsx
+++ src/components/ZoomPhoto/ZoomPhoto.jsx
@@ -6,6 +6,8 @@ import axios from "axios";
 //CSS and ICONS
 import "./ZoomPhoto.css";
 import { IoCloseSharp } from "react-icons/io5";
+import { FaDownload } from "react-icons/fa";
+import { FaInfo } from "react-icons/fa";
 
 const ZoomPhoto = ({ photo, setPhotoZoom }) => {
   const imageDownloadRef = useRef(null);
@@ -50,13 +52,41 @@ const ZoomPhoto = ({ photo, setPhotoZoom }) => {
         <img src={photo.urls.regular} alt={photo.alt_description} />
         <a ref={imageDownloadRef} className="hidden"></a>
 
-        <div>
-          <p>Nome: {photo.user.name}</p>
-          <button onClick={() => setPhotoZoom(null)}>
+        <div className="info">
+          <button onClick={() => setPhotoZoom(null)} className="button-close">
             <IoCloseSharp />
           </button>
+          <h3>
+            Informations <FaInfo />
+          </h3>
+          <ul>
+            <li>
+              <span>Description</span>: {photo.alt_description}
+            </li>
+            <li>
+              <span>Location</span>:{" "}
+              {photo.location && photo.location.name
+                ? photo.location.name
+                : "Location not available"}
+            </li>
+            <li>
+              <span>Photo by:</span>{" "}
+              <a href={photo.user.links.html} target="_blank">
+                {photo.user.name}
+              </a>{" "}
+              on{" "}
+              <a href="https://unsplash.com/pt-br" target="_blank">
+                Unsplash
+              </a>
+            </li>
+          </ul>
 
-          <button onClick={() => downloadImage(photo)}>Download</button>
+          <button
+            onClick={() => downloadImage(photo)}
+            className="download-button"
+          >
+            Download <FaDownload />
+          </button>
         </div>
       </div>
     </div>
