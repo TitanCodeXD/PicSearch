bccfbef797bf9f9187c500b1a74c9b43cf6b78aa
Wesley Santos <wesleysantos32892653@outlook.com>
2024-07-06 21:16:51 -0300

    Aumentando exibição de imagens

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -44,7 +44,7 @@ function App() {
     const response = await axios.get("https://api.unsplash.com/photos/random", {
       params: {
         client_id: apiKey,
-        count: 8,
+        count: 9,
       },
     });
 
